1a31623bcedf4cebeac5319086ffd8c3b2d6958f
Vahid Abdi <vahidabdinet@gmail.com>
2017-09-24 12:24:16 +0330

    Change migration

--- priv/repo/migrations/20170914141603_create_account_users.exs
+++ priv/repo/migrations/20170914141603_create_account_users.exs
@@ -1,8 +1,7 @@
 defmodule Escala.Repo.Migrations.CreateAccountUsers do
   use Ecto.Migration
 
-  def up do
-    execute "CREATE EXTENSION IF NOT EXISTS \"uuid-ossp\" WITH SCHEMA public;"
+  def change do
     create table(:account_users, primary_key: false) do
       add :id, :uuid, primary_key: true
       add :email, :text, null: false
@@ -18,6 +17,4 @@ defmodule Escala.Repo.Migrations.CreateAccountUsers do
     create unique_index(:account_users, [:email])
     create unique_index(:account_users, [:username])
   end
-
-  def down, do: drop table(:account_users)
 end
